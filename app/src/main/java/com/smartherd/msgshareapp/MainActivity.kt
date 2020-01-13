273f5df283587b2099e3dec78c7e3718af41738a
shadslattery <shadslattery@gmail.com>
2020-01-13 15:56:09 -0500

    Section Two, Video Two

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.smartherd.msgshareapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ProgressDialog.show
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            // Code
+            Log.i("MainActivity", "Button was clicked !")
+
+        Toast.makeText(this,"Button was clicked !",Toast.LENGTH_SHORT).show()
+        }
+
     }
-
-
 
 }
